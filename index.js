ec0ffea49862683ee0eea8a4a39d2d883ef1eca5
Holly Teunis <+>
2021-10-13 23:44:29 -0400

    commit tree 96616bb7be4fcbf4e3d8a9553dc9104d2cfea224

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function receivesAFunction (callback) {
+    return callback()
+}
+
+  function returnsANamedFunction(){
+    return function test(){
+    }
+  }
+  
+  
+  function returnsAnAnonymousFunction(){
+    return function(){
+    }
+  }
